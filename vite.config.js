d7ff03753d687dd5d02c36a097fda4ed0892b4b0
unknown <lhn1208@naver.com>
2024-12-17 19:10:23 +0900

    업로드

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import path from 'path';
 export default defineConfig({
   plugins: [vue()],
   resolve: {
+    base: '/vue-vet-app/',
     alias: {
       '@': path.resolve(__dirname, './src'), // '@'를 'src' 폴더로 설정
     },
