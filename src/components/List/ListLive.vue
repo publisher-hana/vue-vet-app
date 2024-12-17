9deef9efe01ded0b0f488984cd9efa63ba7ed207
unknown <lhn1208@naver.com>
2024-12-17 19:50:29 +0900

    이미지 경로 수정

--- src/components/List/ListLive.vue
+++ src/components/List/ListLive.vue
@@ -1,19 +1,19 @@
   <script setup>
   import ListLiveItem from './ListLiveItem.vue'
-
+  import ImgLive from '/src/assets/images/thum_live_w277.jpg'
   </script>
   <template>
     <div class="list_style col"> 
       <ul class="grid g_3col">
         <ListLiveItem 
           label="호흡기질환"  
-          img="@/assets/images/thum_live_w277.jpg"
+          :img="ImgLive"
           title="2022 석천나눔재간 헬스케어 연구지원 및 연구실정발표 연구지원 및 연구실정발표 연구지원 "
           date="2023.01.26 13:00~14:00"
         />
         <ListLiveItem 
           label="호흡기질환"  
-          img="@/assets/images/thum_live_w277.jpg"
+          :img="ImgLive"
           title="2023 석천나눔재간 헬스케어 연구지원 및 연구실정발표 연구지원 및 연구실정발표 연구지원 "
           date="2023.01.26 13:00~14:00"
         />
